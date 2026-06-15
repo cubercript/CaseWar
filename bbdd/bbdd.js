a2878cbff86504741cb34adcfd8f92d0a2f93411
Jesus <jfortea.fatsini@gmail.com>
2026-06-15 22:55:35 +0200

    BBDD.js -> Armas predefinidas implementadas

--- bbdd/bbdd.js
+++ bbdd/bbdd.js
@@ -12,7 +12,20 @@ document.addEventListener("DOMContentLoaded", () => {
 
         // Nombre arma - Daño - Valor - Tipo - Rareza //
         
-        ["ejemplo", 255, 15, "Anti-terrestre", "Común"],
+        ["Beretta M9", 25, 10, "Media distancia", "Común"],
+        ["CZ75 - Champion", 45, 80, "Media distancia", "Raro"],
+        ["Desert eagle", 75, 45, "Media distancia", "Común"],
+        ["Glock 18", 20, 8, "Media distancia", "Común"],
+        ["Kriss Vector", 135, 100, "Media distancia", "Común"],
+        ["MAC 10", 95, 65, "Media distancia", "Común"],
+        ["Magnum", 35, 15, "Media distancia", "Común"],
+        ["MP5 - SD", 80, 50, "Media distancia", "Común"],
+        ["MP7", 110, 70, "Media distancia", "Común"],
+        ["P90", 120, 80, "Media distancia", "Común"],
+        ["P250 - Blackout", 35, 220, "Media distancia", "Raro"],
+        ["PP Bizon - Nostromo", 100, 110, "Media distancia", "Raro"],
+        ["UMP 45", 95, 60, "Media distancia", "Común"],
+        ["USP S", 20, 8, "Media distancia", "Común"],
 
     ];
 
