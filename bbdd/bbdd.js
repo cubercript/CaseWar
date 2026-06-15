4bf925dd553d579b63eadc7c5e6acdf17e640043
Jesus <jfortea.fatsini@gmail.com>
2026-06-15 13:19:45 +0200

    Base del proyecto -> Ficheros, estructura, links y predefiniciones de valores

---
+++ bbdd/bbdd.js
@@ -0,0 +1,23 @@
+// ==========================================================================
+//                          SIMULADOR DE BBDD MEDIANTE JS
+// ==========================================================================
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    var armas = [];
+    
+    
+    // -- Todas las armas
+    armas = [
+
+        // Nombre arma - Daño - Valor - Tipo - Rareza //
+        
+        ["ejemplo", 255, 15, "Anti-terrestre", "Común"],
+
+    ];
+
+
+
+})
+
+
