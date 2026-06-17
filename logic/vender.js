a9c9d39c39c0baa61d5f2380cdb4b12c959b199d
Jesus <jfortea.fatsini@gmail.com>
2026-06-17 17:38:09 +0200

    vender.js -> Implementación de la lógica de venta

--- logic/vender.js
+++ logic/vender.js
@@ -2,6 +2,17 @@
 //                                  VENDER ARMAS
 // ==========================================================================
 
+venderArmaButton.onclick = () => {
 
+    // --> Sumamos dinero al banco
+    bank = Number(bank) + itemWinnerRandom[2];
+    money.innerHTML = "Dinero: " + bank + "€";
+
+    
+    // --> Activamos y desactivamos botones
+    tirarCajaOn();
+    selectArmaOff();
+    
+}
 
 
