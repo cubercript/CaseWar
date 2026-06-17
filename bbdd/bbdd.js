dd955717c31b77d8065d581826745216eb6df1f8
Jesus <jfortea.fatsini@gmail.com>
2026-06-17 12:55:42 +0200

    tirarCaja.js -> Implementación de la lógica de la ruleta (totalmente funcional)

--- bbdd/bbdd.js
+++ bbdd/bbdd.js
@@ -2,35 +2,29 @@
 //                          SIMULADOR DE BBDD MEDIANTE JS
 // ==========================================================================
 
-document.addEventListener("DOMContentLoaded", () => {
+var armas = [];
 
-    var armas = [];
+
+// -- Todas las armas
+armas = [
+
+    // Nombre arma - Daño - Valor - Tipo - Rareza - Imagen //
     
-    
-    // -- Todas las armas
-    armas = [
+    ["Beretta M9", 25, 10, "Media distancia", "Común", "beretta-m9.png"],
+    ["CZ75 - Champion", 45, 80, "Media distancia", "Raro", "cz75-champion.png"],
+    ["Desert eagle", 75, 45, "Media distancia", "Común", "desert-eagle.png"],
+    ["Glock 18", 20, 8, "Media distancia", "Común", "glock-18.png"],
+    ["Kriss Vector", 135, 100, "Media distancia", "Común", "kriss-vector.png"],
+    ["MAC 10", 95, 65, "Media distancia", "Común", "mac-10.png"],
+    ["Magnum", 35, 15, "Media distancia", "Común", "magnum.png"],
+    ["MP5 - SD", 80, 50, "Media distancia", "Común", "mp5-sd.png"],
+    ["MP7", 110, 70, "Media distancia", "Común", "mp7.png"],
+    ["P90", 120, 80, "Media distancia", "Común", "p90.png"],
+    ["P250 - Blackout", 35, 220, "Media distancia", "Raro", "p250-blackout.png"],
+    ["PP Bizon - Nostromo", 100, 110, "Media distancia", "Raro", "pp-bizon-nostromo.png"],
+    ["UMP 45", 95, 60, "Media distancia", "Común", "ump-45.png"],
+    ["USP S", 20, 8, "Media distancia", "Común", "usp-s.png"],
 
-        // Nombre arma - Daño - Valor - Tipo - Rareza //
-        
-        ["Beretta M9", 25, 10, "Media distancia", "Común"],
-        ["CZ75 - Champion", 45, 80, "Media distancia", "Raro"],
-        ["Desert eagle", 75, 45, "Media distancia", "Común"],
-        ["Glock 18", 20, 8, "Media distancia", "Común"],
-        ["Kriss Vector", 135, 100, "Media distancia", "Común"],
-        ["MAC 10", 95, 65, "Media distancia", "Común"],
-        ["Magnum", 35, 15, "Media distancia", "Común"],
-        ["MP5 - SD", 80, 50, "Media distancia", "Común"],
-        ["MP7", 110, 70, "Media distancia", "Común"],
-        ["P90", 120, 80, "Media distancia", "Común"],
-        ["P250 - Blackout", 35, 220, "Media distancia", "Raro"],
-        ["PP Bizon - Nostromo", 100, 110, "Media distancia", "Raro"],
-        ["UMP 45", 95, 60, "Media distancia", "Común"],
-        ["USP S", 20, 8, "Media distancia", "Común"],
-
-    ];
-
-
-
-})
+];
 
 
--- logic/variables.js
+++ logic/variables.js
@@ -3,4 +3,56 @@
 // ==========================================================================
 
 
+// --> Header
+var info = document.getElementById("info");
+var colection = document.getElementById("colection");
+var store = document.getElementById("store");
+
+
+// --> Stats
+var bank = document.getElementById("bank").value;
+var money = document.getElementById("money");
+var resets = document.getElementById("resets").value;
+var totalResets = document.getElementById("totalResets");
+
+
+// --> Ruleta
+var boxRoulette = document.getElementById("box-roulette");
+var roulette = document.getElementById("roulette");
+
+
+// --> Botones
+var tirarCajaButton = document.getElementById("tirarCaja");
+var venderArmaButton = document.getElementById("venderArma");
+var lucharButton = document.getElementById("luchar");
+
+
+
+// --> Estadísticas de tabla
+var weapon = document.getElementById("weapon");
+var damage = document.getElementById("damage");
+var value = document.getElementById("value");
+var type = document.getElementById("type");
+
+
+
+// =================================
+//               BOT
+// =================================
+
+// --> Ruleta
+var botBoxRoulette = document.getElementById("bot-box-roulette");
+var boxRoulette = document.getElementById("bot-roulette");
+
+
+// --> Estadísticas de tabla
+var botWeapon = document.getElementById("bot-weapon");
+var botDamage = document.getElementById("bot-damage");
+var botValue = document.getElementById("bot-value");
+var botType = document.getElementById("bot-type");
+
+
+
+
+
 
